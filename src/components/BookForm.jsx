55bdcfad73d79f385edd58589474ade7269ed00f
Kev-Tan <kevinsutan99@gmail.com>
2026-01-30 13:07:14 +0700

    Add form to accept user input to add books
    Deleted button to add static content. Add form to accept user input to add new books to list

--- src/components/BookForm.jsx
+++ src/components/BookForm.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const BookForm = () => {
+const BookForm = ({books, setBooks}) => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [genre, setGenre] = useState("");
+  const [country, setCountry] = useState("");
+  const [synopsis, setSynopsis] = useState("");
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    const book = {title, author, genre, country, synopsis}
+    console.log(book)
+    setBooks([...books, book])
+    }
+  
+// A thriller set in a submarine as they race to fix a combustion engine that will blow up in 2 hours
+
   return (
-    <div>BookForm</div>
-  )
-}
+    <div className="flex justify-center">
+      <form className="border flex flex-col w-[400px] m-4 p-8 gap-3" onSubmit={handleSubmit}>
+        {/* Row */}
+        <div className="flex items-center">
+          <label className="w-24 font-semibold">Book:</label>
+          <input
+            className="flex-1 border px-2 py-1"
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className="flex items-center">
+          <label className="w-24 font-semibold">Author:</label>
+          <input
+            className="flex-1 border px-2 py-1"
+            type="text"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          />
+        </div>
+        <div className="flex items-center">
+          <label className="w-24 font-semibold">Genre:</label>
+          <input
+            className="flex-1 border px-2 py-1"
+            type="text"
+            value={genre}
+            onChange={(e) => setGenre(e.target.value)}
+          />
+        </div>
+        <div className="flex items-center">
+          <label className="w-24 font-semibold">Country:</label>
+          <input
+            className="flex-1 border px-2 py-1"
+            type="text"
+            value={country}
+            onChange={(e) => setCountry(e.target.value)}
+          />
+        </div>
+        <div className="flex items-start">
+          <label className="w-24 font-semibold pt-1">Synopsis:</label>
+          <textarea
+            className="flex-1 border px-2 py-1"
+            rows="3"
+            value={synopsis}
+            onChange={(e) => setSynopsis(e.target.value)}
+          />
+        </div>
+        <button className="bg-white text-black hover:bg-emerald-400 hover:text-white border duration-100 ease-in transition-colors">
+          Add book
+        </button>{" "}
+      </form>
+    </div>
+  );
+};
 
-export default BookForm
+export default BookForm;
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -2,32 +2,19 @@
 import { useState } from 'react'
 import React from 'react'
 import BookItem from './BookItem'
-
+import BookForm from './BookForm'
 
 
 
 
 const BookList = ({books, setBooks}) => {
 
-    
-    let randObj = {
-    id: 6,
-    title: "Pride and Prejudice",
-    author: "Jane Austen",
-    country: "United Kingdom",
-    genre: "Classic Romance",
-    synopsis: "A witty and insightful novel that explores love, social class, and personal growth through the evolving relationship between Elizabeth Bennet and the reserved Mr. Darcy."
-    }
-
 
   return (
     <div className='p-8'>        
-        {/* TODO: Modify button so it can take in user input instead */}
-        <button className='mb-3' onClick={()=>{
-            console.log("adding books")
-            setBooks([...books, randObj])
 
-        }}>Add</button>
+        <BookForm books={books} setBooks={setBooks}/>
+
         <div className='min-w-md bg-slate-200 p-5'>
             {/* {createBookItem(books[0])}
             {createBookItem(books[1])}
